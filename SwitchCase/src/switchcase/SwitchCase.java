a33aa9c6f5412fe91a2f64235bce912109e3e4d9
MiawElecat <hilmi.af05@gmail.com>
2024-01-29 13:07:00 +0700

    Create SwitchCase.java

---
+++ SwitchCase/src/switchcase/SwitchCase.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package switchcase;
+
+/**
+ *
+ * @author HILMI
+ */
+class SwitchCase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { int bulan = 4;
+        switch (bulan) {
+        
+            case 1: System.out.println("pertama");
+            case 2: System.out.println("kedua");
+            case 3: System.out.println("kuartal 1");
+            break;
+            case 4: System.out.println("keempat");
+            case 5:
+            case 6: System.out.println("kuartal 2");
+            case 7:
+            case 8:
+            case 9: System.out.println("kuartal 3");
+            break;
+            default: System.out.println("kuartal 4");
+        }
+            System.out.println("keluar dari switch");
+    }
+    
+}
